fd6cbc4bcf008e8f8d11c2afbafeba1551ca32ee
ibk_temi <ibk12mails@gmail.com>
2020-05-19 13:50:42 +0100

    added the sign up component

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './form-input.styles.scss';
 
-const FormInput = ({ handleChange, label, ...otherProps}) => {
+const FormInput = ({ handleChange, label, validationErrorMessage, ...otherProps}) => {
   return (
     <div className='group'>
       <input className="form-input" onChange={handleChange} {...otherProps} />
@@ -12,6 +12,7 @@ const FormInput = ({ handleChange, label, ...otherProps}) => {
           </label>
         ) : (null)
       }
+      <span>{validationErrorMessage}</span>
     </div>
   )
 }
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,6 +13,32 @@ const fbConfig = {
   appId: "1:943160265157:web:00d7c08f9f7f97109663ac"
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      })
+    } catch (error) {
+      console.log('error creating user', error.message)
+    }
+  }
+
+  return userRef;
+
+}
+
 firebase.initializeApp(fbConfig);
 
 export const auth = firebase.auth();
